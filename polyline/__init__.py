3b8323b6400cefdf2c3db43aa09c4499f8a8975d
ASM <cpp.create@gmail.com>
2016-04-27 23:31:29 +0300

    flake8 fix

--- polyline/__init__.py
+++ polyline/__init__.py
@@ -1,10 +1,13 @@
+from .codec import PolylineCodec
 __version__ = '1.2.1'
 
-from .codec import PolylineCodec
+
 def decode(expression, precision=5):
     return PolylineCodec().decode(expression, precision)
-    
+
+
 def encode(coordinates, precision=5):
     return PolylineCodec().encode(coordinates, precision)
+
 
 __all__ = ['decode', 'encode']
