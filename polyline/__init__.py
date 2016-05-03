f2391d3f76c51aeffe3b27423ee6170a5c286bee
ASMfreaK <cpp.create@gmail.com>
2016-05-03 20:17:52 +0300

    Created a nicer interface for package (#2)
    * Created a nicer interface for package

--- polyline/__init__.py
+++ polyline/__init__.py
@@ -1,1 +1,13 @@
+from .codec import PolylineCodec
 __version__ = '1.2.1'
+
+
+def decode(expression, precision=5):
+    return PolylineCodec().decode(expression, precision)
+
+
+def encode(coordinates, precision=5):
+    return PolylineCodec().encode(coordinates, precision)
+
+
+__all__ = ['decode', 'encode']
