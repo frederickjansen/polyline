b5753cbb7567ad9b1634b85aef0ab507dae06a08
Aaron Seelye <aseelye@i.farm>
2019-05-19 07:07:20 -0700

    add support for lonlat, as per GeoJSON spec

--- polyline/__init__.py
+++ polyline/__init__.py
@@ -15,6 +15,18 @@ def decode(expression, precision=5):
     return PolylineCodec().decode(expression, precision)
 
 
+def decode_lonlat(expression, precision=5):
+    """
+    Decode a polyline string into a set of lonlat coordinates.
+
+    :param expression: Polyline string, e.g. 'u{~vFvyys@fS]'.
+    :param precision: Precision of the encoded coordinates. Google Maps uses 5, OpenStreetMap uses 6.
+        The default value is 5.
+    :return: List of coordinate tuples
+    """
+    return PolylineCodec().decode_lonlat(expression, precision)
+
+
 def encode(coordinates, precision=5):
     """
     Encode a set of coordinates in a polyline string.
@@ -27,4 +39,16 @@ def encode(coordinates, precision=5):
     return PolylineCodec().encode(coordinates, precision)
 
 
-__all__ = ['decode', 'encode']
+def encode_lotlat(coordinates, precision=5):
+    """
+    Encode a set of lonlat coordinates in a polyline string.
+
+    :param coordinates: List of coordinate tuples, e.g. [(0, 0), (1, 0)].
+    :param precision: Precision of the coordinates to encode. Google Maps uses 5, OpenStreetMap uses 6.
+        The default value is 5.
+    :return: The encoded polyline string.
+    """
+    return PolylineCodec().encode_lonlat(coordinates, precision)
+
+
+__all__ = ['decode', 'decode_lonlat', 'encode', 'encode_lotlat']
