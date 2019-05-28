5e4346635fadcffe2fd1812495a59e32811973ac
Aaron Seelye <aseelye@i.farm>
2019-05-28 14:25:43 -0700

    Add support for lon-lat/GeoJSON tuples

--- polyline/__init__.py
+++ polyline/__init__.py
@@ -3,28 +3,30 @@ from .codec import PolylineCodec
 __version__ = '1.3.2'
 
 
-def decode(expression, precision=5):
+def decode(expression, precision=5, geojson=False):
     """
     Decode a polyline string into a set of coordinates.
 
     :param expression: Polyline string, e.g. 'u{~vFvyys@fS]'.
     :param precision: Precision of the encoded coordinates. Google Maps uses 5, OpenStreetMap uses 6.
         The default value is 5.
+    :param geojson: Set output of tuples to (lon, lat), as per https://tools.ietf.org/html/rfc7946#section-3.1.1
     :return: List of coordinate tuples
     """
-    return PolylineCodec().decode(expression, precision)
+    return PolylineCodec().decode(expression, precision, geojson)
 
 
-def encode(coordinates, precision=5):
+def encode(coordinates, precision=5, geojson=False):
     """
     Encode a set of coordinates in a polyline string.
 
     :param coordinates: List of coordinate tuples, e.g. [(0, 0), (1, 0)].
     :param precision: Precision of the coordinates to encode. Google Maps uses 5, OpenStreetMap uses 6.
         The default value is 5.
+    :param geojson: Set to True in order to encode lon-lat tuples.
     :return: The encoded polyline string.
     """
-    return PolylineCodec().encode(coordinates, precision)
+    return PolylineCodec().encode(coordinates, precision, geojson)
 
 
 __all__ = ['decode', 'encode']
